8c1a668ae6becd29de4f59f8fa5b667387fec182
husnainalihaiderjutt <161064154+husnainalihaiderjutt@users.noreply.github.com>
2026-07-21 00:02:32 +0500

    cloudinary setup

---
+++ helpers/Cloudinary.js
@@ -0,0 +1,19 @@
+import { v2 as cloudinary } from 'cloudinary';
+import multer from "multer"
+
+cloudinary.config({
+    cloud_name:process.env.cloud_name,
+    api_key:process.env.api_key,
+    api_secret:process.env.api_secret
+})
+
+const storage = new multer.memoryStorage();
+
+export const handleImageUpload = async(file)=>{
+    const result = await cloudinary.uploader.upload(file,{
+        resource_type: 'auto'
+    });
+    return result;
+}
+export const upload = multer({storage});
+
