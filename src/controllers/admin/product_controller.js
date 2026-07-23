156f914db89146f1ae06ea16576937cc9b6ac94f
husnainalihaiderjutt <161064154+husnainalihaiderjutt@users.noreply.github.com>
2026-07-23 16:13:27 +0500

    creating product model and controllers

--- src/controllers/admin/product_controller.js
+++ src/controllers/admin/product_controller.js
@@ -1,5 +1,5 @@
 import { ImageUploadUtils } from "../../../helpers/Cloudinary.js";
-
+import Product from "../../models/product.js";
 export const handleImageUpload = async(req,res)=>{
     try {
         const b64 = Buffer.from(req.file.buffer).toString('base64');
@@ -18,3 +18,58 @@ export const handleImageUpload = async(req,res)=>{
     });
 }
 };
+
+export const addNewProduct = async(req,res)=>{
+    try {
+       const {image,title,description,category,brand,price,salePrice,totalStock} = req.body
+       const newProduct = new Product({image,title,description,category,brand,price,salePrice,totalStock});
+       await newProduct.save(); 
+       res.status(200).json({success:true,message:"Product Added Successfully"});
+    } catch (error) {
+        res.status(500).json({success:false,message:"Server Error"});
+    }
+}
+
+export const ListProduct = async(req,res)=>{
+    try {
+       const productList = await Product.find({});
+       res.status(200).json({success:true ,data:productList}); 
+    } catch (error) {
+        res.status(500).json({success:false,message:"Server Error"});
+    }
+}
+export const EditProduct = async(req,res)=>{
+    try {
+       const {id} = req.params;
+       const {image,title,description,category,brand,price,salePrice,totalStock} = req.body;
+       const existingProduct = await Product.findById({id});
+       if(!existingProduct)
+       {
+        return res.status(404).json({success:false , message:"Product not found"});
+       }
+       existingProduct.title = title || existingProduct.title;
+       existingProduct.description = description || existingProduct.description;
+       existingProduct.category = category || existingProduct.category;
+       existingProduct.brand = brand || existingProduct.brand;
+       existingProduct.price = price || existingProduct.price;
+       existingProduct.salePrice = price || existingProduct.salePrice;
+       existingProduct.totalStock = price || existingProduct.totalStock;
+       await existingProduct.save();
+       res.status(200).json({success:true,message:"Product Updated Successfully"});
+    } catch (error) {
+        res.status(500).json({success:false,message:"Server Error"});
+    }
+}
+export const DeleteProduct = async(req,res)=>{
+    try {
+      const {id} = req.params;
+      const product = await Product.findByIdAndDelete(id);
+      if(!product)
+        {
+         return res.status(404).json({success:false,message:"Product is not found"});
+        } 
+      res.status(200).json({success:true , message:"Product Deleted Successfully"});
+    } catch (error) { 
+        res.status(500).json({success:false,message:"Server Error"});
+    }
+}
--- src/routes/admin/product_routes.js
+++ src/routes/admin/product_routes.js
@@ -1,8 +1,13 @@
 import express from "express"
-import { handleImageUpload } from "../../controllers/admin/product_controller.js";
+import { handleImageUpload,addNewProduct,DeleteProduct,EditProduct, ListProduct } from "../../controllers/admin/product_controller.js";
 import { upload } from "../../../helpers/Cloudinary.js";
 const router = express.Router();
 
 router.post('/upload-image',upload.single('my_file'),handleImageUpload);
+router.get('/list',ListProduct);
+router.post('/addProduct',addNewProduct);
+router.put('/edit/:id',handleImageUpload);
+router.delete('/delete/:id',handleImageUpload);
+
 
 export default router;
